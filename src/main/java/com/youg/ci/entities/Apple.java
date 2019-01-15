7b88d45bff606c5bc73ac0423385f8cc2b68a437
DESKTOP-EJ7EQ9T\fsy <mg3751986@163.com>
2019-01-15 21:39:02 +0800

    agagin

--- src/main/java/com/youg/ci/entities/Apple.java
+++ src/main/java/com/youg/ci/entities/Apple.java
@@ -2,7 +2,7 @@ package com.youg.ci.entities;
 
 public class Apple {
 	
-	private String appName = "Hello, this is a test program.";
+	private String appName = "Hello, this is a test program...hello";
 	
 	public String getAppName() {
 		return appName;
